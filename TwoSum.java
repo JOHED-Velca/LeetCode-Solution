83279d1c661cbddcc375abbb678a818e44030895
Jonathan Velasquez <johedvelca@gmail.com>
2025-03-20 23:41:41 -0400

    improved project to use same class as leetcode.

--- TwoSum.java
+++ TwoSum.java
@@ -1,37 +1,33 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> myNums = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (myNums.containsKey(complement)) {
+                return new int[] { myNums.get(complement), i };
+            }
+            myNums.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
 
 public class TwoSum {
     public static void main(String[] args) {
 
-        int[] nums = {3, 2, 4};
-        int target = 6;
+        int[] nums1 = {2, 7, 11, 15};
+        int[] nums2 = {3, 2, 4};
+        int[] nums3 = {3, 3};
+        int target1 = 9;
+        int target2 = 6;
+        int target3 = 6;
 
-        HashMap<Integer, Integer> myNumbers = new HashMap<Integer, Integer>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int dif = target - nums[i];
-            if (myNumbers.containsKey(dif)) {
-                System.out.println("[" + myNumbers.get(dif) + "," + i + "]");
-            }
-            myNumbers.put(nums[i], i);
-
-        }
+        Solution sol = new Solution();
+        int[] result = sol.twoSum(nums1, target1);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
     }
 }
-//class Solution {
-//    public int[] twoSum(int[] nums, int target) {
-//        Map<Integer, Integer> myNums = new HashMap<>();
-//
-//        for (int i = 0; i < nums.length; i++) {
-//            int complement = target - nums[i];
-//            if (myNums.containsKey(complement)) {
-//                return new int[] { myNums.get(complement), i };
-//            }
-//            myNums.put(nums[i], i);
-//        }
-//        return new int[0];
-//    }
-//}
+
